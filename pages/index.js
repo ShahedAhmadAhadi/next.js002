645fd04a6c7e5fbf42ca5f9af7a4696a0b738ce9
Shahed Ahmad <shaheda.a16@gmail.com>
2021-11-28 17:58:33 +0430

    404.js added and new Link is used

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 
 function Home() {
     return (
         <div className={styles.container}>
             <h1>Home</h1>
-
+            <Link href="/blog">
+                <a>Blog</a>
+            </Link>
+            <Link href="/product">
+                <a>product</a>
+            </Link>
         </div>
     )
 }
