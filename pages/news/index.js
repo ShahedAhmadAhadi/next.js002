e0c04901ad3d4f5f3825a16ec4561f74ac616b4d
Shahed Ahmad <shaheda.a16@gmail.com>
2021-11-29 21:04:53 +0430

    getServerSideProps added to news route and taken as props in the component successfully

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,11 +1,29 @@
-import React from 'react'
-
-function NewArticleList() {
+function NewArticleList({ article }) {
     return (
-        <h1>
-            List of new articles
-        </h1>
+        <>
+            <h1>
+                List of new articles
+            </h1>
+            {article.map(article => {
+                return (
+                    <div key={article.id}>
+                        <h2>{article.id} {article.title} | {article.catagory} </h2>
+                    </div>
+                )
+            })}
+        </>
     )
 }
 
 export default NewArticleList
+
+export async function getServerSideProps() {
+    const response = await fetch('http://localhost:4000/news')
+    const data = await response.json()
+
+    return {
+        props: {
+            article: data
+        }
+    }
+}
