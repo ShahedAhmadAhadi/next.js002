af78d97c6e37ddf86e3f5f564c0a5764e4ea7a10
Shahed Ahmad <shaheda.a16@gmail.com>
2021-11-29 22:11:23 +0430

    new library added to project and for usage new pages created

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -9,13 +9,22 @@ function Dashboard() {
             const response = await fetch('http://localhost:4000/dashboard')
             const data = await response.json()
             setDashboardData(data)
-            
+            setIsLoading(false)
         }
-    }, [input])
+        fetchDashboardData()
+    }, [])
+
+    if (isLoading) {
+        return <h2>Loading...</h2>
+    }
 
     return (
         <div>
-            
+            <h2>Dashboard</h2>
+            <h2>Posts - {dashboardData.posts}</h2>
+            <h2>Likes - {dashboardData.likes}</h2>
+            <h2>Followers - {dashboardData.followers}</h2>
+            <h2>Following - {dashboardData.following}</h2>
         </div>
     )
 }
