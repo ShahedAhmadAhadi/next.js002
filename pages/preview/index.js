eaa1df00057e9bd4d3b9de716da06e101b24d3d4
Shahed Ahmad <shaheda.a16@gmail.com>
2021-11-30 02:53:34 +0430

    priveiw mode added to project

---
+++ pages/preview/index.js
@@ -0,0 +1,18 @@
+function News({data}) {
+    return (
+        <>
+            <h1>{data}</h1>
+        </>
+    )
+}
+
+export default News
+
+export async function getStaticProps(context) {
+
+    return {
+        props: {
+            data: context.preview ? 'List of draft articles': "List of published articles"
+        }
+    }
+}
