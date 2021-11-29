75a2a81b6b45c12acdf56f2d1347017cc1007ad4
Shahed Ahmad <shaheda.a16@gmail.com>
2021-11-30 00:24:55 +0430

    new api added for the file new to apis

--- pages/events.js
+++ pages/events.js
@@ -1,6 +1,8 @@
 import {useState} from 'react'
+import { useRouter } from 'next/router'
 
 function EventList({ eventList }) {
+    const router = useRouter()
 
     const [events, setEvents] = useState(eventList)
 
@@ -9,6 +11,7 @@ function EventList({ eventList }) {
         const data = await response.json()
 
         setEvents(data)
+        router.push('/events?category=sports', undefined, { shallow: true })
     }
 
     return (
@@ -35,8 +38,11 @@ function EventList({ eventList }) {
 
 export default EventList
 
-export async function getServerSideProps() {
-    const response = await fetch(`http://localhost:4000/events`)
+export async function getServerSideProps(context) {
+    const {query} = context
+    const {category} = query
+    const queryString = category ? 'category=sports': ''
+    const response = await fetch(`http://localhost:4000/events?${queryString}`)
     const data = await response.json()
 
     return {
