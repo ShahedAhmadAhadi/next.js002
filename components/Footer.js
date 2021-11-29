fd481fd76398a7155c8ce0fc5a080b50674a49c0
Shahed Ahmad <shaheda.a16@gmail.com>
2021-11-30 01:52:52 +0430

    new layout added to project and also the page defined separate layouts too

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <div className="layout-header">
+        <div className="layout-footer">
             Footer
         </div>
     )
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,16 @@
+import Footer from "../components/Footer"
+
 function About() {
     return <h1>About Page</h1>
 }
 
 export default About
+
+About.getLayout = function PageLayout(page) {
+    return (
+        <>
+            {page}
+            <Footer />
+        </>
+    )
+}
